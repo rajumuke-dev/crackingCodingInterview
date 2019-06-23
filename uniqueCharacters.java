f6aafb1de8230c7a698b52e99a94e5b2e77eba71
rajumuke-dev <51832944+rajumuke-dev@users.noreply.github.com>
2019-06-23 09:43:38 +0530

    added a comment to clarify the solution

--- uniqueCharacters.java
+++ uniqueCharacters.java
@@ -1,4 +1,12 @@
 public class IsUnique {
+	
+	//approach : We are silply using indexOf function to check the existense of character
+	//if character exits then funtion indexOf function will return "1" else "0";
+	
+	//Note :  In order to avoid conflict between upperCase and lowerCase letter make sure
+	//yo can first convert whole string in either upper & lower case and then
+	//apply function. for example String s1= "rajuMUKE" it will return "rajuMUKE" as it is
+	// as a ascii value is different for lower case 'u' & 'U'.
 
 	public static void main(String[] args) {
 		
